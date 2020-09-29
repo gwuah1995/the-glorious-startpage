80c02f29f3c064e63fe1d95abd83dd454243d3ed
Khoa Tran <dktran@protonmail.com>
2020-09-29 15:23:30 +0200

    Add rest of websites

--- js/config.js
+++ js/config.js
@@ -112,16 +112,64 @@ class Config {
 				category: 'development'
 			},
 			{
+				site: 'Home Assistant',
+				icon: 'homeassistant',
+				url: 'http://192.168.1.48:8123',
+				category: 'home'
+			},
+			{
+				site: 'Ikea',
+				icon: 'ikea',
+				url: 'https://www.ikea.com/nl/nl/profile/login/',
+				category: 'shop'
+			},
+			{
 				site: 'Instagram',
 				icon: 'instagram',
 				url: 'https://instagram.com/',
 				category: 'social'
 			},
 			{
+				site: 'Kickstarter',
+				icon: 'kickstarter',
+				url: 'https://www.kickstarter.com/login?ref=nav',
+				category: 'shop'
+			},
+			{
+				site: 'Kraket',
+				icon: 'kraket',
+				url: 'https://kraket.nl/en',
+				category: 'social'
+			},
+			{
+				site: 'Lichess',
+				icon: 'lichess',
+				url: 'https://lichess.org/',
+				category: 'games'
+			},
+			{
+				site: 'Lidl Plus',
+				icon: 'lidlplus',
+				url: 'https://www.lidl-shop.nl/',
+				category: 'shop'
+			},
+			{
+				site: 'LinkedIn',
+				icon: 'linkedin',
+				url: 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin',
+				category: 'social'
+			},
+			{
 				site: 'MEGA',
 				icon: 'mega',
-				url: 'https://mega.nz/',
-				category: 'cloud'
+				url: 'https://mega.nz',
+				category: 'security'
+			},
+			{
+				site: 'MyPUP',
+				icon: 'mypup',
+				url: 'https://login.mypup.nl/Login?ReturnUrl=%2F',
+				category: 'shop'
 			},
 			{
 				site: 'Netflix',
@@ -130,10 +178,52 @@ class Config {
 				category: 'media'
 			},
 			{
+				site: 'NordVPN',
+				icon: 'nordvpn',
+				url: 'https://nordvpn.com',
+				category: 'security'
+			},
+			{
+				site: 'NS',
+				icon: 'ns',
+				url: 'https://login.ns.nl/login',
+				category: 'travel'
+			},
+			{
 				site: 'Office 365',
 				icon: 'office365',
 				url: 'https://office.com/',
 				category: 'information'
+			},
+			{
+				site: 'OV-chipkaart',
+				icon: 'ovchipkaart',
+				url: 'https://www.ov-chipkaart.nl/home.htm#/',
+				category: 'travel'
+			},
+			{
+				site: 'Overleaf',
+				icon: 'overleaf',
+				url: 'https://www.overleaf.com/login',
+				category: 'development'
+			},
+			{
+				site: 'PayPal',
+				icon: 'paypal',
+				url: 'https://www.paypal.com/signin?country.x=nl&locale.x=nl_nl',
+				category: 'finance'
+			},
+			{
+				site: 'PostNL',
+				icon: 'postnl',
+				url: 'https://jouw.postnl.nl/account/nl-NL/overzicht/track-and-trace',
+				category: 'shop'
+			},
+			{
+				site: 'Protonmail',
+				icon: 'protonmail',
+				url: 'https://mail.protonmail.com/login',
+				category: 'security'
 			},
 			{
 				site: 'Reddit',
@@ -142,15 +232,51 @@ class Config {
 				category: 'social'
 			},
 			{
+				site: 'SimpleNote',
+				icon: 'simplenote',
+				url: 'https://app.simplenote.com/signin',
+				category: 'security'
+			},
+			{
+				site: 'Soundcloud',
+				icon: 'soundcloud',
+				url: 'https://soundcloud.com/',
+				category: 'media'
+			},
+			{
+				site: 'Tele2',
+				icon: 'tele2',
+				url: 'https://www.tele2.nl/mijn-tele2/',
+				category: 'shop'
+			},
+			{
+				site: 'Tweakers',
+				icon: 'tweakers',
+				url: 'https://tweakers.net/my.tnet/login/?location=https%3a//tweakers.net/',
+				category: 'media'
+			},
+			{
 				site: 'Twitch',
 				icon: 'twitch',
 				url: 'https://twitch.tv/',
 				category: 'media'
 			},
 			{
+				site: 'VUNet',
+				icon: 'vunet',
+				url: 'https://vunet.login.vu.nl',
+				category: 'school'
+			},
+			{
 				site: 'Youtube',
 				icon: 'youtube',
-				url: 'https://youtube.com/',
+				url: 'https://www.youtube.com/feed/subscriptions',
+				category: 'media'
+			},
+			{
+				site: 'Ziggogo',
+				icon: 'ziggogo',
+				url: 'https://www.ziggogo.tv/nl.html',
 				category: 'media'
 			}
 		];
